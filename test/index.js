928ea1c9edd57333f1e7f1b13a4ab4fe127a4b92
Alexander Pope <alex@pope-industries.com>
2019-10-10 08:25:07 +0200

    add prettier

--- test/index.js
+++ test/index.js
@@ -16,11 +16,11 @@ describe('Inline Source CLI', () => {
   });
 
   it('should inline and compress the file properly', async () => {
-    await exec('node dist/index.js --compress test/fixtures/foo.html test/out/foo.html');
+    await exec(
+      'node dist/index.js --compress test/fixtures/foo.html test/out/foo.html'
+    );
     // check the file
     const data = await readFile('test/out/foo.html', 'utf8');
-    expect(data).to.equal(
-      '<script>var foo=this;console.log(foo);</script>\n'
-    );
+    expect(data).to.equal('<script>var foo=this;console.log(foo);</script>\n');
   });
 });
